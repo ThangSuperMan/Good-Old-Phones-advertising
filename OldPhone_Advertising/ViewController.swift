c2da84812cff440c4bbfbeeef284b6f2754aaddc
ThangSuperMan <thangjenny2002@gmail.com>
2021-06-30 10:47:42 +0700

    Initial Commit

---
+++ OldPhone_Advertising/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OldPhone_Advertising
+//
+//  Created by Phan Tan Thang on 6/30/21.
+//  Copyright © 2021 Phan Tan Thang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
